c340b8c0396b4948c5fb10183e7f0f61c26d664d
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-03-07 02:52:14 +0100

    fix: regenerate front side of the project

--- extranet/src/index.tsx
+++ extranet/src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
