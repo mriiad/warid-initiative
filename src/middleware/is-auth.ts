f82f2075866f2b43f2f2f0928fad5c3aa1ceecc0
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-07-15 03:17:51 +0200

    recode `is-auth` handling all response types

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,5 +1,5 @@
 import { NextFunction } from 'express';
-import { User } from '../models/user';
+import { BaseError } from '../utils/errors/baseError';
 const config = require('../../config.json');
 
 const jwt = require('jsonwebtoken');
@@ -9,15 +9,23 @@ export const isAuth = async (
 	res: Response,
 	next: NextFunction
 ) => {
-	try {
-		const { token } = req.cookies;
-		if (!token) {
-			return next('Please login to access the data');
-		}
-		const verify = await jwt.verify(token, config.authConfig.SECRET_KEY);
-		req.user = await User.findById(verify.id);
-		next();
-	} catch (error) {
-		return next(error);
+	const authHeader = req.headers.get('Authorization'); // `Bearer ${token}` sent by the client side
+	if (!authHeader) {
+		const error = new BaseError(401, 'Not authenticated.');
+		throw error;
 	}
+	const token = authHeader.split(' ')[1];
+	let decodedToken;
+	try {
+		decodedToken = jwt.verify(token, config.authConfig.SECRET_KEY);
+	} catch (err: any) {
+		err.statusCode = 500;
+		throw err;
+	}
+	if (!decodedToken) {
+		const error = new BaseError(401, 'Not authenticated.');
+		throw error;
+	}
+	req.userId = decodedToken.userId;
+	next();
 };
