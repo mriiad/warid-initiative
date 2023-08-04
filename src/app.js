23ac6ed242d7acff4af6ab2679af2928bc7f0a95
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-08-05 00:49:56 +0200

    fix status code issue

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
+const donationRouter = require('./routes/donation');
 
 const config = require('../config.json');
 
