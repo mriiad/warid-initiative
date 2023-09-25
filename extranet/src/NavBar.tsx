4474f557dfdba7e982049bde042c87f0b01acb34
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-09-25 02:04:17 +0200

    Add login button style

--- extranet/src/NavBar.tsx
+++ extranet/src/NavBar.tsx
@@ -1,4 +1,4 @@
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import Button from '@mui/material/Button';
 import { makeStyles } from '@mui/styles';
 import { Link, useNavigate } from 'react-router-dom';
@@ -9,6 +9,7 @@ const Contact = () => <div>Contact Page</div>;
 
 const useStyles = makeStyles({
 	navbar: {
+		padding: '0 170px',
 		display: 'flex',
 		position: 'fixed',
 		alignItems: 'center',
@@ -51,15 +52,27 @@ const useStyles = makeStyles({
 		textDecoration: 'none',
 	},
 	loginButton: {
-		backgroundColor: '#ff3067 !important',
-		justifyContent: 'space-between',
-		paddingLeft: '1.1em',
-		paddingRight: '1.1em',
-		color: 'white',
-		display: 'flex',
-		alignItems: 'center',
-		height: '40px',
-		width: 'auto',
+		backgroundColor: 'rgba(255,255,255,.3)',
+		padding: '8px',
+		borderWidth: '0.8px',
+		overflow: 'visible',
+		border: '1px solid #fff',
+		borderRadius: '1.5625em',
+		'& > button': {
+			backgroundColor: '#ff3067 !important',
+			justifyContent: 'space-between',
+			paddingLeft: '1.1em',
+			paddingRight: '1.1em',
+			color: 'white',
+			display: 'flex',
+			alignItems: 'center',
+			height: '40px',
+			width: 'auto',
+			boxShadow: '0 15px 30px rgba(255,48,103,.3)',
+			'&.MuiButtonBase-root': {
+				borderRadius: '16px',
+			},
+		},
 	},
 	loginIcon: {
 		fontSize: '1.2rem',
@@ -68,44 +81,54 @@ const useStyles = makeStyles({
 });
 
 const Navbar = () => {
-	const classes = useStyles();
+	const {
+		navbar,
+		routes,
+		routesList,
+		routesListItem,
+		routesLink,
+		logo,
+		loginButton,
+		loginIcon,
+	} = useStyles();
 
 	const navigate = useNavigate();
 
 	return (
-		<div className={classes.navbar}>
-			<div className={classes.logo}>
+		<div className={navbar}>
+			<div className={logo}>
 				<img src='logo.png' alt='Logo' />
 			</div>
-			<div className={classes.routes}>
+			<div className={routes}>
 				<nav>
-					<ul className={classes.routesList}>
-						<li className={classes.routesListItem}>
-							<Link to='/' className={classes.routesLink}>
+					<ul className={routesList}>
+						<li className={routesListItem}>
+							<Link to='/' className={routesLink}>
 								Home
 							</Link>
 						</li>
-						<li className={classes.routesListItem}>
-							<Link to='/about' className={classes.routesLink}>
+						<li className={routesListItem}>
+							<Link to='/about' className={routesLink}>
 								About
 							</Link>
 						</li>
-						<li className={classes.routesListItem}>
-							<Link to='/contact' className={classes.routesLink}>
+						<li className={routesListItem}>
+							<Link to='/contact' className={routesLink}>
 								Contact
 							</Link>
 						</li>
 					</ul>
 				</nav>
 			</div>
-			<Button
-				variant='contained'
-				className={classes.loginButton}
-				startIcon={<ArrowForwardIcon className={classes.loginIcon} />}
-				onClick={() => navigate('/login')}
-			>
-				Login
-			</Button>
+			<div className={loginButton}>
+				<Button
+					variant='contained'
+					startIcon={<ArrowCircleRightIcon className={loginIcon} />}
+					onClick={() => navigate('/login')}
+				>
+					Login
+				</Button>
+			</div>
 		</div>
 	);
 };
