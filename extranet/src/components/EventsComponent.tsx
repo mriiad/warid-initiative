b99036799aea42bba6fb305be64960148cc3c6c8
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-10-02 00:25:10 +0200

    Add fallback on loading & styling the cards' list on display

--- extranet/src/components/EventsComponent.tsx
+++ extranet/src/components/EventsComponent.tsx
@@ -1,10 +1,23 @@
-import { Button, Card, CardContent, Typography } from '@mui/material';
+import {
+	Button,
+	Card,
+	CardContent,
+	CircularProgress,
+	Typography,
+} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../auth/AuthContext';
 
 const useStyles = makeStyles({
+	eventsContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		gap: '20px',
+		justifyContent: 'center',
+	},
 	eventCard: {
 		backgroundSize: 'cover',
 		borderRadius: '20px',
@@ -12,8 +25,18 @@ const useStyles = makeStyles({
 		position: 'relative',
 		overflow: 'hidden',
 		marginBottom: '20px',
-		'&:hover': {
-			transform: 'scale(1.05)',
+		transform: 'translateX(100%)',
+		opacity: 0,
+		animation: '$slideIn 0.5s forwards ease-out',
+	},
+	'@keyframes slideIn': {
+		'0%': {
+			transform: 'translateX(100%)',
+			opacity: 0,
+		},
+		'100%': {
+			transform: 'translateX(0)',
+			opacity: 1,
 		},
 	},
 	title: {
@@ -31,20 +54,30 @@ const useStyles = makeStyles({
 		fontSize: 'clamp(12px, 1.2vw, 16px)',
 		fontWeight: 'bold',
 	},
+	fallBack: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		minHeight: '100vh',
+	},
 });
 
 const EventsComponent = () => {
 	const { token } = useAuth();
 
-	const classes = useStyles();
+	const { eventsContainer, title, subTitle, eventCard, content, fallBack } =
+		useStyles();
 	const [events, setEvents] = useState([]);
 
 	const [page, setPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(0);
 
+	const [isLoading, setIsLoading] = useState(false);
+
 	useEffect(() => {
 		const fetchEvents = async () => {
 			try {
+				setIsLoading(true); // Set loading to true when starting to fetch
 				const response = await axios.get(
 					`http://localhost:3000/api/events?page=${page}`,
 					{
@@ -52,55 +85,67 @@ const EventsComponent = () => {
 					}
 				);
 				setEvents(response.data.events);
-				setTotalPages(Math.ceil(response.data.totalItems / 5)); // Assuming 5 is the number of events per page.
+				setTotalPages(Math.ceil(response.data.totalItems / 5));
 			} catch (error) {
 				console.error('Error fetching events', error);
+			} finally {
+				setIsLoading(false); // Reset loading to false after fetching is done
 			}
 		};
 
 		fetchEvents();
-	}, [page, token]); // Re-fetch when page number changes.
+	}, [page, token]);
 
 	console.log('events', events);
 
 	return (
 		<div>
-			{events.map((event) => (
-				<Card
-					key={event._id}
-					className={classes.eventCard}
-					style={{
-						backgroundImage: `url(data:image/jpeg;base64,${event.image})`,
-					}}
-				>
-					<CardContent>
-						<Typography variant='h5' className={classes.title}>
-							{event.title}
-						</Typography>
-						<div className={classes.content}>
-							<Typography variant='body2' className={classes.subTitle}>
-								{event.subtitle}
-							</Typography>
-							<Typography variant='body2'>
-								Location: {event.location}
-							</Typography>
-							<Typography variant='body2'>
-								Date: {new Date(event.date).toLocaleDateString()}
-							</Typography>
-							<Typography variant='body2'>
-								Map:{' '}
-								<a
-									href={event.mapLink}
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									Link
-								</a>
-							</Typography>
-						</div>
-					</CardContent>
-				</Card>
-			))}
+			{isLoading ? (
+				<div className={fallBack}>
+					<CircularProgress />
+				</div>
+			) : (
+				<div className={eventsContainer}>
+					{events.map((event, index) => (
+						<Card
+							key={event._id}
+							className={eventCard}
+							style={{
+								backgroundImage: `url(data:image/jpeg;base64,${event.image})`,
+								animationDelay: `${index * 0.2}s`,
+							}}
+						>
+							<CardContent>
+								<Typography variant='h5' className={title}>
+									{event.title}
+								</Typography>
+								<div className={content}>
+									<Typography variant='body2' className={subTitle}>
+										{event.subtitle}
+									</Typography>
+									<Typography variant='body2'>
+										Location: {event.location}
+									</Typography>
+									<Typography variant='body2'>
+										Date: {new Date(event.date).toLocaleDateString()}
+									</Typography>
+									<Typography variant='body2'>
+										Map:{' '}
+										<a
+											href={event.mapLink}
+											target='_blank'
+											rel='noopener noreferrer'
+										>
+											Link
+										</a>
+									</Typography>
+								</div>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			)}
+			{/* TODO: work on list display (horizontal cards maybe) & pagination (on scroll or with btns) */}
 			<div>
 				<Button disabled={page === 1} onClick={() => setPage(page - 1)}>
 					Previous
