7e3340ce7a89d1c99f42acf056cbb8a4fc9b3975
MOHAMED RIAD <mohamedriiad@gmail.com>
2023-10-12 02:26:51 +0200

    Inherit style from the NavBar component

--- extranet/src/components/MobileNavbar.tsx
+++ extranet/src/components/MobileNavbar.tsx
@@ -2,44 +2,62 @@ import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import EventIcon from '@mui/icons-material/Event';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../auth/AuthContext';
+import colors from '../styles/colors';
+
+const useStyles = makeStyles({
+	navbar: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		height: '60px',
+		backgroundColor: 'rgba(255, 255, 255, .3)',
+		backdropFilter: 'blur(50px)',
+		borderTop: '0.8px solid #fff',
+		position: 'fixed',
+		width: '100%',
+		bottom: 0,
+		left: 0,
+		zIndex: 100,
+		'-webkit-backdrop-filter': 'blur(50px)',
+	},
+	icon: {
+		color: '#3B2A82',
+	},
+	activeIcon: {
+		color: colors.rose,
+	},
+});
 
 const MobileNavbar = () => {
 	const { token, isAdmin } = useAuth();
-	const navigate = useNavigate();
+	const { icon, activeIcon } = useStyles();
 	const location = useLocation();
-	const [value, setValue] = React.useState(location.pathname);
-
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-		navigate(newValue);
-	};
+	const currentRoute = location.pathname;
 
 	return (
-		<BottomNavigation value={value} onChange={handleChange} showLabels>
-			<BottomNavigationAction label='Home' value='/' icon={<HomeIcon />} />
-			<BottomNavigationAction
-				label='About'
-				value='/about'
-				icon={<InfoIcon />}
-			/>
-			<BottomNavigationAction
-				label='Events'
-				value='/events?page=1'
-				icon={<EventIcon />}
-			/>
-			{token && isAdmin && (
-				<BottomNavigationAction
-					label='Admin'
-					value='/admin'
-					icon={<AdminPanelSettingsIcon />}
+		<div className={useStyles().navbar}>
+			<Link to='/'>
+				<HomeIcon className={currentRoute === '/' ? activeIcon : icon} />
+			</Link>
+			<Link to='/about'>
+				<InfoIcon className={currentRoute === '/about' ? activeIcon : icon} />
+			</Link>
+			<Link to='/events?page=1'>
+				<EventIcon
+					className={currentRoute.startsWith('/events') ? activeIcon : icon}
 				/>
+			</Link>
+			{token && isAdmin && (
+				<Link to='/admin'>
+					<AdminPanelSettingsIcon
+						className={currentRoute === '/admin' ? activeIcon : icon}
+					/>
+				</Link>
 			)}
-		</BottomNavigation>
+		</div>
 	);
 };
 
