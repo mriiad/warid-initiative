55ad1c8aa7e2fc74f307536bc53f933d2efa5b60
MOHAMED RIAD <mohamedriiad@gmail.com>
2024-02-02 03:59:32 +0100

    Add contact-us route and optional token check middleware to allow route for all users

--- src/routes/feed.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-/**
- * Could contain news & other data from different resources (Event)
- */
-const router = Router();
-
-router.get('/', (req, res, next) => {});
-
-export default router;
