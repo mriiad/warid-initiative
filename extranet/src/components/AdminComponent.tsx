752fc8a89864a6dcd7674175eb6a769b676d5634
Lazrak-chaymae <cho66el@gmail.com>
2024-02-18 03:48:52 +0100

    Add Display users button

--- extranet/src/components/AdminComponent.tsx
+++ extranet/src/components/AdminComponent.tsx
@@ -5,9 +5,16 @@ export default function AdminComponent() {
 	const navigate = useNavigate();
 
 	return (
+		<div>
 		<ActionButton
 			title='Add Event'
 			onClick={() => navigate('/events/create')}
 		/>
+       
+		<ActionButton
+			title='Display users'
+			onClick={() => navigate('/users?page=1')}
+		/>
+		</div>
 	);
 }
