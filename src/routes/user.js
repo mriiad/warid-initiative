74449a9ae8cce2895c33a6b999dc35f83d9df5b8
MOHAMED RIAD <mohamedriiad@gmail.com>
2024-05-19 17:17:29 +0200

    Revert "[Back] - Implement a New Search Endpoint"
    This reverts commit e2691a32af53e793a5239abd448abde3938f90be.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const userRouter = express.Router();
 const { isAuth } = require('../middleware/token-check');
-const checkIfAdmin = require('../utils/checks');
 const {
 	getUsers,
 	updateUserInfo,
 	checkUserProfile,
 	getProfile,
-	searchUserByUsername,
 } = require('../controllers/user');
 
+// Define a route to display users to admin
 userRouter.get('/api/users', getUsers);
 
 userRouter.put('/api/user/update', isAuth, updateUserInfo);
@@ -17,12 +16,5 @@ userRouter.put('/api/user/update', isAuth, updateUserInfo);
 userRouter.get('/api/user/check-profile', isAuth, checkUserProfile);
 
 userRouter.get('/api/user/profile', isAuth, getProfile);
-
-userRouter.get(
-	'/api/searchUsers/:username',
-	isAuth,
-	checkIfAdmin,
-	searchUserByUsername
-);
 
 module.exports = userRouter;
