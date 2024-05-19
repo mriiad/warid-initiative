ec5925c4298e74aa03307a735e6b1dcef8869c69
MOHAMED RIAD <mohamedriiad@gmail.com>
2024-05-19 17:13:26 +0200

    [Back] - Implement a New Search Endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const userRouter = express.Router();
 const { isAuth } = require('../middleware/token-check');
+const checkIfAdmin = require('../utils/checks');
 const {
 	getUsers,
 	updateUserInfo,
 	checkUserProfile,
 	getProfile,
+	searchUserByUsername,
 } = require('../controllers/user');
 
-// Define a route to display users to admin
 userRouter.get('/api/users', getUsers);
 
 userRouter.put('/api/user/update', isAuth, updateUserInfo);
@@ -16,5 +17,12 @@ userRouter.put('/api/user/update', isAuth, updateUserInfo);
 userRouter.get('/api/user/check-profile', isAuth, checkUserProfile);
 
 userRouter.get('/api/user/profile', isAuth, getProfile);
+
+userRouter.get(
+	'/api/searchUsers/:username',
+	isAuth,
+	checkIfAdmin,
+	searchUserByUsername
+);
 
 module.exports = userRouter;
