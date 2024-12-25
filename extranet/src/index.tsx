c43677d9fffb6f991db8bc5253e4730f3f05128c
Lazrak-chaymae <cho66el@gmail.com>
2024-12-25 01:46:42 +0100

    update index file
    wrap app with I18nextProvider

--- extranet/src/index.tsx
+++ extranet/src/index.tsx
@@ -6,6 +6,8 @@ import App from './App';
 import { AuthProvider } from './auth/AuthContext';
 import './index.css';
 import theme from './theme';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './translation/i18n';
 
 const queryClient = new QueryClient();
 
@@ -14,7 +16,9 @@ ReactDOM.render(
 		<QueryClientProvider client={queryClient}>
 			<ThemeProvider theme={theme}>
 				<AuthProvider>
-					<App />
+				   <I18nextProvider i18n={i18n}>
+                      <App />
+                   </I18nextProvider>
 				</AuthProvider>
 			</ThemeProvider>
 		</QueryClientProvider>
