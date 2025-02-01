e43d7c547840239d0ac70ed5bead67eef50b41fd
Lazrak-chaymae <cho66el@gmail.com>
2025-02-01 14:41:18 +0100

    Add confirmed attribute to attendees in event model
    Introduced the confirmed attribute to the attendees array to track whether an attendee has confirmed their participation.

--- src/models/event.js
+++ src/models/event.js
@@ -1,50 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 /**
  * An Event that can be attended by many users
  */
 
 const EventSchema = new Schema({
-	reference: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	title: {
-		type: String,
-		required: true,
-	},
-	subtitle: {
-		type: String,
-		required: false,
-	},
-	image: {
-		type: Buffer,
-		required: false,
-	},
-	location: {
-		type: String,
-		required: true,
-	},
-	date: {
-		type: Date,
-		required: true,
-	},
-	mapLink: {
-		type: String,
-		required: false,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	attendees: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-			required: false,
-		},
-	],
+  reference: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: Buffer,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  mapLink: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  attendees: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      confirmed: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
 });
 
-module.exports = mongoose.model('Event', EventSchema);
+module.exports = mongoose.model("Event", EventSchema);
