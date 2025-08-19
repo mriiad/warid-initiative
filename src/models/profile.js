09e52782b61e7290ba9d20c5c0855764124a004f
Lazrak-chaymae <cho66el@gmail.com>
2025-08-19 02:54:10 +0100

    keep a single cities list and remove duplicate

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { cities }  = require('../utils/utils');
+const { cities }  = require('../utils/cities');
 
 
 const profileSchema = new Schema({
